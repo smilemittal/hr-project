9d32123a3dfbb622e012f468cd026e8ddbe2aa3f
your usernamenavjottechnaitra <navjot.technaitra@gmail.com>
2020-09-02 14:07:11 +0530

    actions button

--- resources/views/admin/employee/index.blade.php
+++ resources/views/admin/employee/index.blade.php
@@ -67,13 +67,29 @@
                                                 <td> {{$employee->contact_number}}</td>
                                                 <td>{{$employee->created_at->diffForHumans()}}</td>
                                                 <td>
-                                                    <a href="{{route('employee-detail' ,  encrypt($employee->id))}}"
+                                                    <span class="dropdown">
+                                                        <button id="btnSearchDrop12" type="button" class="btn btn-sm btn-icon btn-pure font-medium-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <i class="ft-more-vertical"></i>
+                                                        </button>
+                                                        <span aria-labelledby="btnSearchDrop12" class="dropdown-menu mt-1 dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(55px, 27px, 0px);">
+                                      
+                                                            <a href="{{route('employee-detail' ,  encrypt($employee->id))}}" class="dropdown-item" title="update the value">
+                                                                <i class="ft-plus-circle primary"></i>View</a>
+                                                   
+                                                            <a href="{{route('update-employee' ,  encrypt($employee->id))}}" class="dropdown-item" title="Edit the value">
+                                                                <i class="ft-trash-2"></i> Edit</a>
+                                                            <a href="{{route('delete-employee' ,  encrypt($employee->id))}}" class="dropdown-item" title="Danger! this action will delete the record from database">
+                                                                <i class="ft-edit-2"></i> Delete</a>
+                                                    
+                                                        </span>
+                                                    </span>
+                                                    {{--<a href="{{route('employee-detail' ,  encrypt($employee->id))}}"
                                                        class="btn btn-primary mr-2" title="update the value">View</a>
                                                     <a href="{{route('update-employee' ,  encrypt($employee->id))}}"
                                                        class="btn btn-dark mr-2" title="update the value">Update</a>
                                                     <a href="{{route('delete-employee' ,  encrypt($employee->id))}}"
                                                        class="btn btn-danger mr-2"
-                                                       title="Danger! this action will delete the record from database">Delete</a>
+                                                       title="Danger! this action will delete the record from database">Delete</a>--}}
                                                 </td>
                                             </tr>
                                             @php
