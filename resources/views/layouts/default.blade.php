70fc5a4d45398b45a12454d49a3eec7b136939fe
navjottechnaitra <navjottechnaitra>
2020-09-19 05:46:27 -0700

    server desgin changes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -58,7 +58,7 @@
     <div class="navbar-wrapper">
         <div class="navbar-container content">
             <div class="collapse navbar-collapse show" id="navbar-mobile">
-                <ul class="nav navbar-nav mr-auto float-left">
+                <!-- <ul class="nav navbar-nav mr-auto float-left">
                     {{--                    <li class="nav-item mobile-menu d-md-none mr-auto">--}}
                     {{--                        <a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#">--}}
                     {{--                            <i class="ft-menu font-large-1"> </i>--}}
@@ -225,7 +225,217 @@
                     {{--                            </li>--}}
                     {{--                        </ul>--}}
                     {{--                    </li>--}}
-                </ul>
+                </ul> -->
+                <ul class="nav navbar-nav float-left">
+                        <li class="nav-item mobile-menu d-md-none mr-auto"><a
+                                class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
+                                    class="ft-menu font-large-1"></i></a></li>
+                      
+                        <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i
+                                    class="ficon ft-maximize"></i></a></li>
+                        <li class="dropdown nav-item mega-dropdown d-none d-md-block"><a
+                                class="dropdown-toggle nav-link" href="#" data-toggle="dropdown">Mega</a>
+                            <ul class="mega-dropdown-menu dropdown-menu row">
+                                <li class="col-md-2">
+                                    <h6 class="dropdown-menu-header text-uppercase mb-1"><i class="ft-link"></i> Quick
+                                        Links</h6>
+                                    <ul>
+                                        <li><a class="my-1" href="chat-application.html"><i class="ft-home"></i>
+                                                Chat</a></li>
+                                        <li><a class="my-1" href="table-bootstrap.html"><i class="ft-grid"></i>
+                                                Tables</a></li>
+                                        <li><a class="my-1" href="chartist-charts.html"><i class="ft-bar-chart"></i>
+                                                Charts</a></li>
+                                        <li><a class="my-1" href="gallery-grid.html"><i class="ft-sidebar"></i>
+                                                Gallery</a></li>
+                                    </ul>
+                                </li>
+                                <li class="col-md-3">
+                                    <h6 class="dropdown-menu-header text-uppercase mb-1"><i class="ft-star"></i> My
+                                        Bookmarks</h6>
+                                    <ul class="ml-2">
+                                        <li class="list-style-circle"><a class="my-1" href="card-bootstrap.html">
+                                                Cards</a></li>
+                                        <li class="list-style-circle"><a class="my-1" href="full-calender.html">
+                                                Calender</a></li>
+                                        <li class="list-style-circle"><a class="my-1" href="invoice-template.html">
+                                                Invoice</a></li>
+                                        <li class="list-style-circle"><a class="my-1" href="users-contacts.html">
+                                                Contact</a></li>
+                                    </ul>
+                                </li>
+                                <li class="col-md-3">
+                                    <h6 class="dropdown-menu-header text-uppercase"><i class="ft-layers"></i> Recent
+                                        Products</h6>
+                                    <div class="carousel slide pt-1" id="carousel-example" data-ride="carousel">
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="carousel-item active"><img class="d-block w-100"
+                                                    src="app-assets/images/carousel/08.jpg" alt="First slide"></div>
+                                            <div class="carousel-item"><img class="d-block w-100"
+                                                    src="app-assets/images/carousel/03.jpg" alt="Second slide"></div>
+                                            <div class="carousel-item"><img class="d-block w-100"
+                                                    src="app-assets/images/carousel/01.jpg" alt="Third slide"></div>
+                                        </div><a class="carousel-control-prev" href="#carousel-example" role="button"
+                                            data-slide="prev"><span class="la la-angle-left"
+                                                aria-hidden="true"></span><span class="sr-only">Previous</span></a><a
+                                            class="carousel-control-next" href="#carousel-example" role="button"
+                                            data-slide="next"><span class="la la-angle-right icon-next"
+                                                aria-hidden="true"></span><span class="sr-only">Next</span></a>
+                                        <h5 class="pt-1">Special title treatment</h5>
+                                        <p>Jelly beans sugar plum.</p>
+                                    </div>
+                                </li>
+                                <li class="col-md-4">
+                                    <h6 class="dropdown-menu-header text-uppercase mb-1"><i class="ft-thumbs-up"></i>
+                                        Get in touch</h6>
+                                    <form class="form form-horizontal pt-1">
+                                        <div class="form-body">
+                                            <div class="form-group row">
+                                                <label class="col-sm-3 form-control-label" for="inputName1">Name</label>
+                                                <div class="col-sm-9">
+                                                    <div class="position-relative has-icon-left">
+                                                        <input class="form-control" id="inputName1" type="text"
+                                                            placeholder="John Doe">
+                                                        <div class="form-control-position pl-1"><i class="ft-user"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-3 form-control-label"
+                                                    for="inputContact1">Contact</label>
+                                                <div class="col-sm-9">
+                                                    <div class="position-relative has-icon-left">
+                                                        <input class="form-control" id="inputContact1" type="text"
+                                                            placeholder="(123)-456-7890">
+                                                        <div class="form-control-position pl-1"><i
+                                                                class="ft-smartphone"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-3 form-control-label"
+                                                    for="inputEmail1">Email</label>
+                                                <div class="col-sm-9">
+                                                    <div class="position-relative has-icon-left">
+                                                        <input class="form-control" id="inputEmail1" type="email"
+                                                            placeholder="john@example.com">
+                                                        <div class="form-control-position pl-1"><i class="ft-mail"></i>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-3 form-control-label"
+                                                    for="inputMessage1">Message</label>
+                                                <div class="col-sm-9">
+                                                    <div class="position-relative has-icon-left">
+                                                        <textarea class="form-control" id="inputMessage1" rows="2"
+                                                            placeholder="Simple Textarea"></textarea>
+                                                        <div class="form-control-position pl-1"><i
+                                                                class="ft-message-circle"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-sm-12 mb-1">
+                                                    <button class="btn btn-danger float-right" type="button"><i
+                                                            class="ft-arrow-right"></i> Submit</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown d-none d-md-block mr-1"><a class="dropdown-toggle nav-link"
+                                id="apps-navbar-links" href="#" data-toggle="dropdown">
+                                Apps</a>
+                            <div class="dropdown-menu">
+                                <div class="arrow_box"><a class="dropdown-item" href="email-application.html"><i
+                                            class="ft-user"></i> Email</a><a class="dropdown-item"
+                                        href="chat-application.html"><i class="ft-mail"></i> Chat</a><a
+                                        class="dropdown-item" href="project-summary.html"><i class="ft-briefcase"></i>
+                                        Project Summary </a><a class="dropdown-item" href="full-calender.html"><i
+                                            class="ft-calendar"></i> Calendar </a></div>
+                            </div>
+                        </li>
+
+
+
+
+                    </ul>
+                    <ul class="nav navbar-nav float-left custom-search-fild">
+                        <li class="nav-item dropdown navbar-search show"><a class="nav-link dropdown-toggle hide p-0"
+                                data-toggle="dropdown" href="#"><i class="ficon ft-search"></i></a>
+                            <ul class="dropdown-menu p-0">
+                                <li class="arrow_box">
+                                    <form>
+                                        <div class="input-group search-box p-0">
+                                            <div class="position-relative has-icon-right full-width">
+                                                <input class="form-control" id="search" type="text"
+                                                    placeholder="Search here...">
+                                                <div class="form-control-position navbar-search-close"><i
+                                                        class="ft-x"></i></div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+
+
+                    </ul>
+                   <ul class="nav navbar-nav ml-auto float-left mt-1 filter-part">
+                        <li class="nav-item dropdown navbar-search rounded-0">
+                            <div class="btn-group mr-0 mb-1">
+                                <button class="btn btn-danger btn-sm dropdown-toggle" type="button"
+                                    id="dropdownMenuButton6" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Filter
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton6"
+                                    x-placement="bottom-start"
+                                    style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 28px, 0px);">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown navbar-search rounded-0">
+                            <div class="btn-group mr-0 mb-1">
+                                <button class="btn btn-danger btn-sm dropdown-toggle" type="button"
+                                    id="dropdownMenuButton6" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Group By
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton6"
+                                    x-placement="bottom-start"
+                                    style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 28px, 0px);">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown navbar-search rounded-0">
+                            <div class="btn-group mr-0 mb-1">
+                                <button class="btn btn-danger btn-sm dropdown-toggle" type="button"
+                                    id="dropdownMenuButton6" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Favdrfilter
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton6"
+                                    x-placement="bottom-start"
+                                    style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 28px, 0px);">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 <ul class="nav navbar-nav float-right">
                     <li class="dropdown dropdown-language nav-item">
                         <a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown"
